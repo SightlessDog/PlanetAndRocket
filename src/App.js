9834e20dda3c448fdfe77f2bbf02d000d5c5302b
Elyess Eleuch <ely.369@outlook.com>
2022-01-26 13:07:54 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { Canvas } from "@react-three/fiber";
+import {  OrbitControls } from "@react-three/drei";
+import { Suspense } from "react";
+import Model from "./Planet1";
+import Rocket from "./Rocket";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas camera={{position : [0, 0, 50]}}>
+      <ambientLight intensity={1} />
+      <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
+        <Suspense fallback={null}>
+          <Model />
+          <Rocket /> 
+          <OrbitControls />
+        </Suspense>
+      </Canvas>
     </div>
   );
 }
-
-export default App;
